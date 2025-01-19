3cc65e7312948bdd60209934f291f7277e2411e5
infomotin <motin.mmk.jr@gmail.com>
2025-01-19 16:15:26 +0600

    ngrok and prosma model with connect local databases

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 
 import ThemeToggle from "@/components/shared/theme-toggle";
-
 import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
@@ -8,7 +7,7 @@ export default function Home() {
       <div className="w-100 flex gap-x-5 justify-end">
       
       <UserButton/>
-      <ThemeToggle/>
+       <ThemeToggle/>
       </div>
       <h1 className="text-3xl  text-blue-500 font-barlow">Home Page </h1>
      
