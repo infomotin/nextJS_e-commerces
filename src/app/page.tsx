78518a9cd8f2953516fd6bf2067aa1bf2c6956da
infomotin <motin.mmk.jr@gmail.com>
2025-01-19 12:34:58 +0600

    Sing-in/Sign-up /fix Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
 
 import ThemeToggle from "@/components/shared/theme-toggle";
-import { Button } from "@/components/ui/button"
+
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="w-100 flex justify-center">
-      <ThemeToggle>
-        
-      </ThemeToggle>
+    <div className="p-5">
+      <div className="w-100 flex gap-x-5 justify-end">
+      
+      <UserButton/>
+      <ThemeToggle/>
       </div>
-      <h1 className="text-3xl  text-blue-500 font-barlow">Wellcome To Our Cources Page</h1>
-      <Button variant={"destructive"}>Click Me</Button>
+      <h1 className="text-3xl  text-blue-500 font-barlow">Home Page </h1>
+     
     </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isProtectedRoute = createRouteMatcher(['/'])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
@@ -9,4 +17,25 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
+
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { next } from 'next';
+
+
+// export default clerkMiddleware((auth,req,next)=>{
+//   const protectedRoutes = createRouteMatcher(['/']); 
+//   if(protectedRoutes(req)) auth.protect();
+// }
+  
+// );
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
